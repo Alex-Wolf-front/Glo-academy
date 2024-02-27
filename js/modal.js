9cda3adb0e1487d76dce4232d867afd9dc3e46e4
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2024-02-27 16:37:23 +0300

    search input

--- js/modal.js
+++ js/modal.js
@@ -2,10 +2,16 @@ const modal = document.querySelector(".search-model");
 const modalOpen = document.querySelector(".search-switch");
 const modalClose = modal.querySelector(".search-close-switch");
 
-modalOpen.addEventListener("click", () => {
+modalOpen.addEventListener("click", (e) => {
+    e.preventDefault();
     modal.style.display = "block";
 })
 
 modalClose.addEventListener("click", () => {
     modal.style.display = "none";
+})
+
+const searchInput = modal.querySelector('#search-input');
+searchInput.addEventListener('keyup', () => {
+    console.log(searchInput.value)
 })

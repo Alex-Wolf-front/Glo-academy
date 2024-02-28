f86fb0ebe1a9b0dd8a7647c6cd4fa5ccf09de7b8
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2024-02-28 18:51:51 +0300

    genres

--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,15 @@
 const mainData = () => {
+  const renderAnimeList = (array, genre) => {
+    console.log(array);
+    console.log(genre);
+  };
+
   const renderTopAnime = (array) => {
     const wrapper = document.querySelector(".filter__gallery");
     wrapper.innerHTML = "";
     array.forEach((item) => {
-      console.log(item)
-      wrapper.insertAdjacentHTML("beforeend", `
+      wrapper.insertAdjacentHTML("beforeend",
+        `
         <div class="product__sidebar__view__item set-bg" data-setbg="${item.image}">
           <div class="ep">${item.rating} / 10</div>
           <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
@@ -14,7 +19,7 @@ const mainData = () => {
       );
     });
 
-    wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+    wrapper.querySelectorAll(".set-bg").forEach((elem) => {
       elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
     });
   };
@@ -24,8 +29,16 @@ const mainData = () => {
       return response.json();
     })
     .then((data) => {
-      let dataSort = data.sort((a, b) => b.views - a.views).slice(0, 5)
+      const dataSort = data.sort((a, b) => b.views - a.views).slice(0, 5);
+      const genres = new Set();
+
       renderTopAnime(dataSort);
+
+      data.forEach((elem) => {
+        genres.add(elem.ganre);
+      });
+
+      renderAnimeList(data, genres);
     });
 };
 

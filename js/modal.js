2f2ea178b22a19b7603c6a5c2bad9c0d8a0216b5
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2024-02-28 16:21:29 +0300

    function encapsulation

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,21 @@
-const modal = document.querySelector(".search-model");
-const modalOpen = document.querySelector(".search-switch");
-const modalClose = modal.querySelector(".search-close-switch");
+const modal = () => {
+  const modal = document.querySelector(".search-model");
+  const modalOpen = document.querySelector(".search-switch");
+  const modalClose = modal.querySelector(".search-close-switch");
 
-modalOpen.addEventListener("click", (e) => {
+  modalOpen.addEventListener("click", (e) => {
     e.preventDefault();
     modal.style.display = "block";
-})
+  });
 
-modalClose.addEventListener("click", () => {
+  modalClose.addEventListener("click", () => {
     modal.style.display = "none";
-})
+  });
 
-const searchInput = modal.querySelector('#search-input');
-searchInput.addEventListener('keyup', () => {
-    console.log(searchInput.value)
-})
+  const searchInput = modal.querySelector("#search-input");
+  searchInput.addEventListener("keyup", () => {
+    console.log(searchInput.value);
+  });
+};
+
+modal();
--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,10 +1,14 @@
-const scrollBtn = document.querySelector("#scrollToTopButton");
-scrollBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  
-  seamless.scrollIntoView(document.querySelector(".header"), {
-    behavior: "smooth",
-    block: "center",
-    inline: "center",
+const scrollToTop = () => {
+  const scrollBtn = document.querySelector("#scrollToTopButton");
+  scrollBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    seamless.scrollIntoView(document.querySelector(".header"), {
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
   });
-});
+};
+
+scrollToTop();

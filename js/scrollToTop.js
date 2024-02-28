52823dde04c09decb16ad842712d6baae0dc5b39
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2024-02-28 16:08:42 +0300

    scrollToTop

---
+++ js/scrollToTop.js
@@ -0,0 +1,10 @@
+const scrollBtn = document.querySelector("#scrollToTopButton");
+scrollBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  seamless.scrollIntoView(document.querySelector(".header"), {
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
+});
